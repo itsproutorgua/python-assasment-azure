12b35dd2ed3c5ed4cf62a9bb1dc2015ef91047b2
Arsen-hrynevych <102324797+Arsen-hrynevych@users.noreply.github.com>
2023-05-03 15:02:36 +0200

    fix in main.py

--- arsen_hrynevych/main.py
+++ arsen_hrynevych/main.py
@@ -1,38 +1,28 @@
-import logging
+import telebot
+from flask import Flask, request, Response
 from api import API
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler
-from flask import Flask, request
+
+bot = telebot.TeleBot(API)
 
 app = Flask(__name__)
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-
-@app.route('/webhooks', methods=['POST'])
+@app.route('/',  methods=['POST'])
 def webhook_handler():
     if request.method == 'POST':
-        update = request.get_json()
-        application.process_update(update)
-    return 'ok'
+        update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+        bot.process_new_updates([update])
+        return Response('ok', status=200)
+    
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "HI")
+
+
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Please continue")
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(API).build()
-
-    application.add_handler(CommandHandler('start', lambda update, context: context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me")))
-
-    application.add_handler(
-        MessageHandler(filters.TEXT & (~filters.COMMAND), lambda update, context: context.bot.send_message(
-            chat_id=update.effective_chat.id, text=update.message.text)))
-
-    application.add_handler(MessageHandler(filters.COMMAND, lambda update, context: context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")))
-    
-    WEBHOOK_URL = "https://9879509a-4f0e-4afe-88fe-f98cf5b44996.webhook.we.azure-automation.net/webhooks?token=3tA7lGLWnep7VceAKFXaGyqWrGZ9DQILJ5od8MtNoCE%3d"
-    application.set_webhook(WEBHOOK_URL)
     app.run()
-
